b25f8e792413ff0713c57f95291a83702db809f1
haoyan.zhang <haoyan.zhang@qunar.com>
2018-11-12 20:51:47 +0800

    1

--- src/com/jinjisousuo/Bag.java
+++ src/com/jinjisousuo/Bag.java
@@ -1,8 +1,40 @@
 package com.jinjisousuo;
 
 public class Bag {
-    int currentWeight = 0;
-    int currentValue = 0;
-    int totalWeight;
-    int totalValue;
+
+    private int currentWeight;
+
+    private int currentValue;
+
+    private int totalWeight;
+
+    public Bag(int totalWeight){
+        this.totalWeight = totalWeight;
+        this.currentValue= 0 ;
+        this.currentWeight = 0;
+    }
+
+    public int getCurrentWeight() {
+        return currentWeight;
+    }
+
+    public void setCurrentWeight(int currentWeight) {
+        this.currentWeight = currentWeight;
+    }
+
+    public int getCurrentValue() {
+        return currentValue;
+    }
+
+    public void setCurrentValue(int currentValue) {
+        this.currentValue = currentValue;
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(int totalWeight) {
+        this.totalWeight = totalWeight;
+    }
 }
